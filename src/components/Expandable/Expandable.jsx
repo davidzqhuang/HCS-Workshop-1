4b718c6d481ee43fde26e30fb14bb477e85ccf49
David Huang <davidhuang@dhcp-10-250-26-247.harvard.edu>
2020-10-10 00:35:19 -0400

    css stuff

--- src/components/Expandable/Expandable.jsx
+++ src/components/Expandable/Expandable.jsx
@@ -8,13 +8,13 @@ export default function Expandable({ content }) {
 	if (clicked) {
 		return (
 			<>
-				<p onClick={() => setClicked(true)}>click for more info</p>
+				<p class="expand_title" onClick={() => setClicked(true)}>click for more info</p>
 				<p> CLICKED!! </p>
 				<p> Here Is Some More Cool Kid Info--the REAL content</p>
 				<p> { content } </p>
 			</>	
 		);
 	} else {
-		return <p onClick={() => setClicked(true)}>click for more info</p>;
+		return <p class="expand_title" onClick={() => setClicked(true)}>click for more info</p>;
 	}
 }
